c96a76b33239b6389d722bd66e52ae17b2caafb8
wackon <jeac86@gmail.com>
2020-04-08 13:49:11 -0500

    cap 2 video 12

--- cap 2/app.js
+++ cap 2/app.js
@@ -1,33 +1,44 @@
-//String - cadenas de texto
 
 nombre = 'juan pablo';
 nombre = "juan pablo"
 //nombre = 'juan pablo"  error 
-mensaje = 'y entonces dije "Buen curso"'; //esta ok
-mensaje = "y entonces dije 'Buen curso!!'"; //esta ok
+
 mensaje = ' y entonces dije \'Buen curso!!\''; //esta ok
 
-// mensaje = 20     // numero
-// mensaje = '20'   // string
 
 
 //concatenar javascript
 
 mensaje = 'JAVA' + 'SCRIPT'
-console.log(mensaje)
+// console.log(mensaje)
 
 //Uniendo variables en una sola
 
-let aprendiendo = 'Aprendiendo',
-    tecnologia = 'javascript'
 
-    //console.log(aprendiendo +" " +tecnologia)
-console.log(`${aprendiendo} ${tecnologia}`)  //template literals
-
+// -------------------------------------------
+// let aprendiendo = 'Aprendiendo',
+//     tecnologia = 'javascript'
 
 
+    //console.log(tecnologia.length) 
 
-// let producto = 'Libro' 
-// producto = 'libro 2'
-// console.log(producto);
 
+//console.log(tecnologia.concat(' ', 'Es genial' ))  
+
+// console.log(tecnologia.toUpperCase());
+
+
+//console.log(tecnologia.toLocaleLowerCase());
+
+// let numero = 300
+// console.log(numero.toLocaleLowerCase()); //sale error ya que el 300 no es un string
+
+
+// let numero = '300'
+// console.log(numero.toLocaleLowerCase()); 
+
+mensaje = 'Aprendiendo javascript. CSS, HTML, paras ser Frontend'
+
+console.log(mensaje.indexOf('javascript'))  // cuensta los caracteres desde donde empieza el mensaje e indica que en la posicion 12 esta el texto javascript
+
+console.log(mensaje.indexOf('PHP'))     //si en consola te sale -1 indica que el texto buscado no se encuentra en el string
