0fab82d8531807e876ef25587867f7a7ca98efed
wackon <jeac86@gmail.com>
2020-04-08 11:31:22 -0500

    cap 2 video 6

--- cap 2/app.js
+++ cap 2/app.js
@@ -1,1 +1,4 @@
-document.getElementById('app').innerHTML = 'Hola mundo'
+let nombre = prompt ('cual es tu nombre ?');
+let edad = prompt('cual es tu edad');
+
+document.getElementById('app').innerHTML = `Bienvenido ${nombre} de ${edad} años`;
