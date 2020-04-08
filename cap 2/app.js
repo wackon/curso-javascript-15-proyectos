b89a5f7e9141f7f5314608f537bfddf121413afd
wackon <jeac86@gmail.com>
2020-04-08 12:08:29 -0500

    cap 2 video 9

--- cap 2/app.js
+++ cap 2/app.js
@@ -1,5 +1,30 @@
-console.log('enviando a la consola');
-console.log(2+2);
-console.log('hasta aqui todo bien');
-console.log(true);
-console.log ('ID :' + 20);
+//var let const
+
+// var nombre = 'Juan'
+// var nombre = 'Jhon';
+
+// var carrito = 'Libro',
+//     carrito1 = 'Libro2';
+
+// console.log(carrito);
+// console.log(carrito1);
+
+
+// var dias99 = 99;
+// console.log(dias99);
+
+//no se pueden numeros despues de una var, let o const ejemm var 99dias =99.
+
+
+// var *hola = "hola";
+// console.log(*hola);
+
+//no se pueden caracteres especiales despues de una var, let o const ejemm var *dias =99
+
+// var nombre = 'Juan';
+
+// Mas de una palabra
+
+var primerNombre = 'Juan'; //camelcase -- recomendada
+var primer_nombre = 'Juan' //underscode
+var PrimerNombre = 'Juan' // pascal case
