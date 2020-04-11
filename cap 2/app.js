20e5383112ec17849cd75d81387f5c7428433a98
wackon <jeac86@gmail.com>
2020-04-10 20:04:25 -0500

    cap 2 video 18

--- cap 2/app.js
+++ cap 2/app.js
@@ -1,46 +1,27 @@
-const numero1 = 20;
-const numero2 = 50;
-const numero3 = '20';
-
-// console.log(numero1 > numero2)
-
-// console.log(numero1 < numero2)
-
-//comparar igual
-
-console.log(20 == '20' )  //revisa solo el valor
-
-console.log(20 === '20' )  //revisa valor y tipo de dato
-
-console.log(numero1 === numero3 );
-
-//Diferentes
-
-console.log(2 != 3);
-
-console.log('hola' == ' hola');
-
-console.log('a' > 'b')
+const numero1 = '50',
+      numero2 = 10,
+      numero3 = 'tres';
 
 
+// console.log( 1 + 2 );
 
-console.log('z' > 'A')
+// console.log('java' + 'Script')
 
+// console.log( numero1 + numero2 );
 
+console.log( typeof Number(numero1)) //Number convierte de string a numero 
 
-//ABCDEFGH........abcdefgh
-
-
-
-console.log( null == undefined)
-
-console.log( null === undefined)
+console.log( parseInt(numero1) + numero2) //parseInt  convierte a String
 
 
+console.log( Number(numero1) + numero2)
 
-let nombre = 'juan'
-let nombre2 = 'Pedro'
+console.log(numero1 - numero2)
 
-//console.log(nombre = nombre2)
+console.log(typeof parseInt(numero1))
 
-console.log(nombre === nombre2)
+
+console.log( typeof Number (numero3));  //el typeof dice que e un numero pero cuando le quitamos el typeof nos da como resultado NAN ( No es un numero )
+
+console.log( Number (numero3));     //el typeof dice que e un numero pero cuando le quitamos el typeof nos da como resultado NAN ( No es un numero )
+
