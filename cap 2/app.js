55cdfc7fe68ecd0be5ff9da66c4bf8717b701441
wackon <jeac86@gmail.com>
2020-04-10 19:26:32 -0500

    cap 2 video 16

--- cap 2/app.js
+++ cap 2/app.js
@@ -1,77 +1,49 @@
-//Numeros en javascript
+// let valor;
 
-// const numero1 = 30
-// const numero2 = 20
-// const numero3 = 20.20
-// const numero4 = .1020
-// const numero5 = -3
+// valor = 'Cadena de texto';
+// valor = 20;
+// valor = true;
+// valor = undefined;
+// valor = {
+//     nombre:'juan'}
+
+// console.log(valor)
+// ------------------------Primitivos---------------
+let valor;
+
+valor = 'Juan';
+valor = 20;
+valor = 20.20
+valor = "20";
+
+//booleanos
+
+valor = true
+valor = false
+
+//null
+valor = null
+
+//symbols
+valor = Symbol('simbolo')
 
 
-//tambien se puede definir asi:
-const numero1 = 30,
-     numero2 = 20,
-     numero3 = 20.20,
-     numero4 = .1020,
-     numero5 = -3;
+////// ----------------------De referencia o tipo objeto----------------
+//arreglo = objeto
+valor = [2,3,4,5,6];
 
 
-//     console.log(numero1)
-  
-let resultado;
-//suma
-resultado = numero1 + numero2;
-//resta
-resultado = numero1 - numero2;
-//multiplicacion
-resultado = numero1 * numero2;
+//Objeto
+valor = {
+    nombre : 'Juan',
+    profesion :'Desarrollador web'
 
-resultado = numero1 * 2;
+}
 
-//divisiones
-resultado = numero1 / numero2
-// Modulo
-resultado = numero1 % numero2
-
-//pi
-resultado = Math.PI;
-
-//redondeo
-resultado = Math.round(2.5);
-    //ceil
-resultado = Math.ceil(2.1);     //redondea hacia arriba
-
-resultado = Math.floor(2.8);    //redondea hacia abajo
-
-//Raiz Cuadrada
-resultado = Math.sqrt(144);
-
-//absoluto
-resultado = Math.abs(numero5);
-
-//potencia
-resultado = Math.pow(8,3);
-
-//numero minimo
-resultado = Math.min(3,5,6,1,7,8)
-
-//numero maximo
-resultado = Math.max(3,5,6,1,7,8)
-
-//aleatorio
-resultado = Math.random()
-
-resultado = 10 + 20 -5
-resultado = 10 + 20 * 5
-resultado = (10 + 20) * 5
+//fecha
+valor = new Date();
 
 
 
-//porcentaje
-resultado = (10 + 20 + 20 + 10 + 40) * .20 ;
 
-console.log(resultado)
-
-let puntaje = 10;
-
-
-console.log(puntaje)
+console.log(typeof valor)
