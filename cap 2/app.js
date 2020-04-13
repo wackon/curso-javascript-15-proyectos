d8622f9939309f2d54c091ab1b238a1193812110
wackon <jeac86@gmail.com>
2020-04-13 15:59:12 -0500

    cap 2 video 32

--- cap 2/app.js
+++ cap 2/app.js
@@ -1,20 +1,44 @@
-//Funcion que no existe
+//Fechas
 
-try{
-    algo()
-}catch(error){
-    console.log(error)
-}finally{
-    console.log('NO le importa, ejecuta de todos modos ')
-}
+//En javascript existe un objeto llamado Date
 
-obtenerClientes()
 
-function obtenerClientes (){
-    console.log('Descargando ....');
+//Fecha en especifico Mes, Dia , Año
+let navidad2017 = new Date('12-25-2017')
+console.log(navidad2017)
 
-    setTimeout(function (){
-        console.log('completo')
-    },3000);
-}
 
+// const diaHoy = new Date();
+// console.log(diaHoy)
+
+
+const diaHoy = new Date();
+
+let valor;
+valor = diaHoy.getMonth();      //Muestra el mes recordando que enero es el mes 0 cero en javascript
+console.log(valor)
+
+valor = diaHoy.getDate();       //Muestra el día
+console.log(valor)
+
+valor = diaHoy.getFullYear();           //Muestra el año
+console.log(valor)
+
+valor = diaHoy.getHours();          //Obtiene la hora en transcurso
+console.log(valor)
+
+valor = diaHoy.getMinutes();        //Obtiene los minutos en transcurso
+console.log(valor)
+
+valor = diaHoy.getSeconds();        //Obtiene los segundos en transcurso
+console.log(valor)
+
+valor = diaHoy.getTime();        //Obtiene los milisegundos desde 1970
+console.log(valor)
+
+
+valor = diaHoy.getFullYear();           //Muestra el año en transcurso
+console.log(valor)
+valor = diaHoy.setFullYear(2030)        //editamos el año a 2030
+valor = diaHoy.getFullYear();
+console.log(valor)
