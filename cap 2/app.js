09031dc36131b72a2db82f3748f0744c6b8ae878
wackon <jeac86@gmail.com>
2020-04-13 15:34:43 -0500

    cap 2 video 31

--- cap 2/app.js
+++ cap 2/app.js
@@ -1,48 +1,20 @@
-//Funciones IIFE
+//Funcion que no existe
 
-// (function(){
-// console.log('Creando un IIFE')
-// })();
-
-
-// function sumar (a,b){
-//     console.log(a + b)
-// }
-// sumar(1,2)
-
-(function(a,b){
-console.log((a * b))
-})(8,3);
-
-(function (tecnologia){
-    console.log(`Aprendiendo ${tecnologia}`)
-})('Javascript')
-
-
-//-----Funciones con Metodos de propiedad-----------------
-
-//Cuando una funcion se pone dentro de un objeto
-
-const musica = {
-    reproducir: function( id ) {
-        console.log(`Reproduciendo Cancion id ${id}`);
-    },
-    
-    pausar: function (){
-        console.log('Pause a la Música')
-    }
+try{
+    algo()
+}catch(error){
+    console.log(error)
+}finally{
+    console.log('NO le importa, ejecuta de todos modos ')
 }
 
+obtenerClientes()
 
+function obtenerClientes (){
+    console.log('Descargando ....');
 
-
-//Los metodos tambien pueden guardarsen o creacsen fuera del objeto
-
-musica.borrar = function(id){
-    console.log(`Borrando la cancion con el ID:${id}`)
+    setTimeout(function (){
+        console.log('completo')
+    },3000);
 }
 
-musica.borrar(2)
-
-musica.reproducir(30)
-musica.pausar()
