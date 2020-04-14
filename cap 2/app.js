d282292117b5fa76dffbe2dfc5716b5e36d0a5b9
wackon <jeac86@gmail.com>
2020-04-14 17:01:51 -0500

    cap 2 video 37 for

--- cap 2/app.js
+++ cap 2/app.js
@@ -1,79 +1,46 @@
-//Switch
+//for loops
+
+// for (let i = 0; i <100; i ++){
+// console.log(`Numero: ${i}`)
+// }
+//-----------------------------------------------------
 
 /*
-const metodoPago = 'bitcoins'
+for (let i = 0; i <10; i ++){
+    if(i==5)
+    {console.log('voy en el cinco')}        //imprime voy en el cinco y a continuación el número 5
+    console.log(`${i}`)
+    }
 
-
-switch (metodoPago){
-
-    case 'efectivo':
-        console.log(`El usuario pago con ${metodoPago}`) 
-        break;
-     
-     case 'cheque':
-        console.log(`El usuario pago con ${metodoPago}`) 
-        break;
-
-     case 'tarjeta':
-        console.log(`El usuario pago con ${metodoPago}`) 
-        break;
-    
-     default :
-        console.log(`metodo de pago no usado`)
-}
-   
+    */
+   // ------------------------------------------------------
+    //------imprime el voy en el cinco y luego el numero 6
+ /*   
+   for (let i = 0; i <10; i ++){
+    if(i==5){
+        console.log('voy en el cinco');    
+        continue;   
+        }
+    console.log(`${i}`)
+    }
 */
+//--------------------------------------------------------
+        //--------------cambiando el continue por el break  solo imprime hasta voy en el cinco y luego sale del for
 
-let mes;
-
-switch(new Date().getMonth()){
-    
-
-    case 0 :
-            mes = `Enero`;
-            break;
-    case 1 :
-        mes = `Febrero`;
-        break;
-    case 2 :
-        mes = `Marzo`;
-        break;
-    case 3 :
-        mes = `Abril`;
-        break;
-    case 4 :
-        mes = `Mayo`;
-        break;
-    case 5 :
-        mes = `Junio`;
-        break;
-    case 6 :
-        mes = `Julio`;
-        break;
-    case 7 :
-        mes = `Agosto`;
-        break;
-    case 8 :
-        mes = `Septiembre`;
-        break;
-    case 9 :
-        mes = `Octubre`;
-        break;
-    case 10 :
-        mes = `Noviembre`;
-        break
-    case 11 :
-            mes = `Diciembre`;
-            break;
-     
-}
-
-console.log(`Este mes es ${mes}`)
+    // for (let i = 0; i <10; i ++){
+    //     if(i==5){
+    //         console.log('voy en el cinco');    
+    //         break;   
+    //         }
+    //     console.log(`${i}`)
+    //     }
 
 
+const arregloProductos = [`Camisa`,`Boletos`,`Guitarra`,`Disco`]        
 
+console.log(arregloProductos.length)
 
-
-
-
-
+for (let i = 0 ; i < arregloProductos.length ; i ++){
+         console.log(`Tu producto  ${arregloProductos[i]} fue agregado`)
+       
+    } 
