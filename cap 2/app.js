8f1e9629a9a38d95e0da2b88e4c45b2997ae9ba5
wackon <jeac86@gmail.com>
2020-04-14 16:38:16 -0500

    cap 2 video 36

--- cap 2/app.js
+++ cap 2/app.js
@@ -1,22 +1,79 @@
-//Operador  or   ||
+//Switch
 
-let efectivo = 300,
-    credito = 300,
-    totalCarrito = 500;
-    disponible = (efectivo + credito)
+/*
+const metodoPago = 'bitcoins'
 
-if(totalCarrito < efectivo || totalCarrito < credito || totalCarrito < disponible){
-    console.log('puedo pagar')
-}else{
-    console.log('No puedo pagar')
+
+switch (metodoPago){
+
+    case 'efectivo':
+        console.log(`El usuario pago con ${metodoPago}`) 
+        break;
+     
+     case 'cheque':
+        console.log(`El usuario pago con ${metodoPago}`) 
+        break;
+
+     case 'tarjeta':
+        console.log(`El usuario pago con ${metodoPago}`) 
+        break;
+    
+     default :
+        console.log(`metodo de pago no usado`)
+}
+   
+*/
+
+let mes;
+
+switch(new Date().getMonth()){
+    
+
+    case 0 :
+            mes = `Enero`;
+            break;
+    case 1 :
+        mes = `Febrero`;
+        break;
+    case 2 :
+        mes = `Marzo`;
+        break;
+    case 3 :
+        mes = `Abril`;
+        break;
+    case 4 :
+        mes = `Mayo`;
+        break;
+    case 5 :
+        mes = `Junio`;
+        break;
+    case 6 :
+        mes = `Julio`;
+        break;
+    case 7 :
+        mes = `Agosto`;
+        break;
+    case 8 :
+        mes = `Septiembre`;
+        break;
+    case 9 :
+        mes = `Octubre`;
+        break;
+    case 10 :
+        mes = `Noviembre`;
+        break
+    case 11 :
+            mes = `Diciembre`;
+            break;
+     
 }
 
-
-//ternario  ---es un if en una sola linea
-
-const logueado = true;
-console.log (logueado === true ? 'si se logueo' : 'No se logueo')
+console.log(`Este mes es ${mes}`)
 
 
-// ? = entonces
-// : = else
+
+
+
+
+
+
