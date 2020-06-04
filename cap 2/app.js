cf086642b14cfac4d9b21dbe7134f0da82506bae
wackon <jeac86@gmail.com>
2020-06-04 15:33:19 -0500

    cap 2 video 41

--- cap 2/app.js
+++ cap 2/app.js
@@ -1,104 +1,45 @@
- const ciudades = ['Londres','New york', 'Madrid','Paris'];
- const ordenes = new Set([123,231,131,102]);
- const datos = new Map();
- datos.set('nombre','juan');
- datos.set('profesion','desarrollador Web');
+// //prompt
 
-//Entries iterador
+// const nombre = prompt();
+// console.log(`Bienvenido ${nombre}`)
 
-// // //Entries a las ciudades
+//Confirm
 
-// for (let entrada of ciudades.entries()){
-//     console.log(entrada);
+// if (confirm('Eliminar?')){
+//     console.log('Eliminado')
+// }else{
+//     console.log('Nada paso!')
 // }
 
 
-//Entries para las ordenes
+// let altura,
+// anchura;
+
+// altura=window.innerHeight,
+// anchura=window.innerWidth;
+
+// console.log (altura);
+// console.log (anchura);
 
 
-// for (let entradas of ordenes.entries()){
-//     console.log(entradas)
-// }
 
-//Entries para el MAP
+//Ubicación
 
-// for (let entrada of datos.entries()){
-//     console.log(entrada)
-// }
+let Ubicación;
 
-//Values a las ciudades
+Ubicación = window.location.search;
 
-// for(let entrada of ciudades.values()){
-//     console.log(entrada)
-// }
+// window.location.href='http://twitter.com'
 
+// window.history.go(-4)
 
-//Values para las ordenes
+//Navigator
+Ubicación =window.navigator.appName
+Ubicación =window.navigator.appVersion
+Ubicación =window.navigator.userAgent
+Ubicación =window.navigator.language
 
 
-// for (let entradas of ordenes.values()){
-//     console.log(entradas)
-// }
-
-//values para el MAP
-
-// for (let entrada of datos.values()){
-//     console.log(entrada)
-// }
-
-//keys iterator
-
-//keys a las ciudades
-
-// for(let entrada of ciudades.keys()){
-//     console.log(entrada)
-// }
+ console.log(Ubicación)
 
 
-//keys para las ordenes
-
-
-// for (let entradas of ordenes.keys()){
-//     console.log(entradas)
-// }
-
-//keys para el MAP
-
-// for (let entrada of datos.keys()){
-//     console.log(entrada)
-// }
-
-//default a las ciudades
-
-// for(let entrada of ciudades){
-//     console.log(entrada)
-// }
-
-//default para las ordenes
-
-
-// for (let entradas of ordenes){
-//     console.log(entradas)
-// }
-
-//default para el MAP
-
-// for (let entrada of datos){
-//     console.log(entrada)
-// }
-
-// const mensaje= 'Aprendiendo javascript'
-
-// for(let i=0; i< mensaje.length;i++){
-//     console.log(mensaje[i])
-// }
-
-// for (let letra of mensaje){
-//     console.log(letra)
-// }
-
-const enlaces = document.getElementsByTagName('a');
-
-for (let enlace of enlaces){
-    console.log(enlace.href);
-}
