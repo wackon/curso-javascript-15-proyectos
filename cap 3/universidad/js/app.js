b149a33b9c358f4ff574d58d91db5da958b06d89
wackon <jeac86@gmail.com>
2020-06-16 17:18:48 -0500

    cap 3 video 45

--- cap 3/universidad/js/app.js
+++ cap 3/universidad/js/app.js
@@ -1,22 +1,60 @@
-//getElementbyid
+//Query Selector
 
-let elemento;
-
-
-
-// encabezado = document.getElementById('encabezado').textContent;
-// encabezado = document.getElementById('encabezado').innerText;
-encabezado = document.getElementById('encabezado');
-encabezado.style.background = '#333'
-encabezado.style.color = '#fff';
-encabezado.style.padding = '20px'
+// const encabezado = document.querySelector('#encabezado')
 
 
-//cambiar texto
-
-encabezado.textContent = 'Los Mejores Cursos'
-
-encabezado.innerText = 'the best couses'
+// //Aplicar css
 
 
-console.log(encabezado.innerText)    
+// encabezado.style.background = '#333';
+// encabezado.style.color = '#fff';
+// encabezado.style.padding = '20px';
+// encabezado.textContent = 'Los Mejores Cursos';
+
+// console.log(encabezado)
+
+
+
+//getelementById solo funciona con id's por lo contrario querySelector funciona con classes y otros atributos para clase seria .
+
+
+// const encabezado = document.querySelector('.encabezado')
+
+
+// encabezado.style.background = '#333';
+// encabezado.style.color = '#fff';
+// encabezado.style.padding = '20px';
+// encabezado.textContent = 'Los Mejores Cursos';
+
+
+
+
+// console.log(encabezado)
+
+/*
+const encabezado = document.querySelector('.enlace')
+
+//si existen muchos elementos con esta clase solo tomará al primero que en cuentre con esa clase.
+
+//querySelectorAll mostrará todos
+
+
+
+console.log(encabezado)
+*/
+
+// const encabezado = document.querySelector('.card img')
+
+
+// console.log(encabezado)
+
+//
+
+let enlace;
+
+enlace = document.querySelector('#principal a:first-child')
+enlace = document.querySelector('#principal a:last-child')
+enlace = document.querySelector('#principal a:nth-child(3)')
+
+
+console.log(enlace)
