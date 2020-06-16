360f6665fc252cfa3274575e92cf154e06201bf1
wackon <jeac86@gmail.com>
2020-06-16 16:21:57 -0500

    cap 3 video 45

--- cap 3/universidad/js/app.js
+++ cap 3/universidad/js/app.js
@@ -1,43 +1,22 @@
+//getElementbyid
+
 let elemento;
 
-elemento = document;
-elemento = document.all;
-elemento = document.all[10];
-elemento = document.head;
-elemento = document.body;
-elemento = document.domain;
-elemento = document.URL;
-elemento = document.characterSet;
-elemento = document.forms;
-elemento = document.forms[0];
-elemento = document.forms[0].id;
-elemento = document.forms[0].className;
-elemento = document.forms[0].classList;
-elemento = document.forms[0].classList[0];
-elemento = document.forms[0].classList;
-elemento = document.forms[0].className;
-elemento = document.forms[0].classList;
-
-elemento = document.images;
-elemento = document.images[2];
-elemento = document.images[2].src;
-elemento = document.images[2].getAttribute;
-elemento = document.images[2].getAttribute('src');
-
-//Scripts
-
- elemento = document.scripts;
 
 
- elemento = document.images;
+// encabezado = document.getElementById('encabezado').textContent;
+// encabezado = document.getElementById('encabezado').innerText;
+encabezado = document.getElementById('encabezado');
+encabezado.style.background = '#333'
+encabezado.style.color = '#fff';
+encabezado.style.padding = '20px'
 
 
-let imagenes= document.images;
-let imagenesArray = Array.from(imagenes);
+//cambiar texto
 
-imagenesArray.forEach(function(imagen){
-    console.log(imagen);
-})
+encabezado.textContent = 'Los Mejores Cursos'
+
+encabezado.innerText = 'the best couses'
 
 
-//console.log(imagenesArray)
+console.log(encabezado.innerText)    
