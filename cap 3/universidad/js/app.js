67a08297087f2eca271a09015a51844a2b56b9e8
wackon <jeac86@gmail.com>
2020-06-25 13:34:54 -0500

    cap 3 video 48

--- cap 3/universidad/js/app.js
+++ cap 3/universidad/js/app.js
@@ -1,46 +1,20 @@
 /*
-let enlaces = document.getElementsByClassName('enlace')[3];
-//enlaces = enlaces[3]
 
-enlaces.style.background = '#333';
-enlaces.textContent = 'Ingenieria'
+const enlaces = document.querySelectorAll('#principal .enlace');
+enlaces[1].style.background ='#000'
+enlaces[1].textContent ='nuevo enlace'
 
 console.log(enlaces)
 */
 
-/*
-const listaEnlaces = document.querySelector('ul')
+const enlaces = document.querySelectorAll('#principal a:nth-child(odd)');
 
-console.log(listaEnlaces)
+//'#principal a: nth-child(odd)' selecciona de principal todos los hijos que son impares
 
-*/
-
-/*
-const listaEnlaces = document.querySelector('#principal').getElementsByClassName('enlace')
-
-console.log(listaEnlaces)
-*/
-
-/*
-
-const links = document.getElementsByTagName('a')
-//ByTagName = todas las etiquetas de html
-links [18].style.color = 'red';
-links [18].textContent = 'INGENIERIA'
-console.log( links )
-*/
-
-/*
-const links = document.getElementsByTagName('a')
-
-let enlaces = Array.from(links);
-
-enlaces.forEach(function(enlace){
-    console.log(enlace.textContent)
+enlaces.forEach(function(impares){
+    impares.style.backgroundColor = 'red';
+    impares.style.color = '#000'
+   
 });
 
-*/
-
-const links = document.getElementsByTagName('h1')
-
-console.log(links)
+console.log(enlaces)
