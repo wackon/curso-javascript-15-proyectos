c70bfd9a833c809be0d4b48d89122b0e1c29863c
wackon <jeac86@gmail.com>
2020-06-26 13:59:52 -0500

    cap 3 video 51

--- cap 3/universidad/js/app.js
+++ cap 3/universidad/js/app.js
@@ -1,57 +1,27 @@
-//traversing del hijo al padre
+//crear enlace
+
+const enlace = document.createElement('a')
+
+// agregamos una clase
+enlace.className = 'enlace'
+
+//añadir id
+enlace.id = 'nuevo-id'
+
+//añadir atributo href
+enlace.setAttribute ('href', '#')
+
+//añadir texto
+ enlace.textContent = 'Nuevo enlace'  //o´
 
 /*
-
-const enlaces = document.querySelectorAll('.enlace')
-
-console.log(enlaces[0].parentNode) //parentNode : lo guia hacia el padre de donde nos encontramos
-console.log(enlaces[0].parentElement)//hace lo mismo que el anterior, pero este es el mas usado
-
-console.log(enlaces[0].parentElement.parentElement.parentElement)//suebe cada vez mas hacia su padre
-
+enlace.appendChild(document.createTextNode('NUEVO ENLACE')); // DA EL MIMO RESULTADO QUE EL ANTERIOR CODIGO
 */
 
-/*
-const cursos = document.querySelectorAll('.card')
 
-console.log(cursos[0])
-console.log(cursos[0].parentElement)
-console.log(cursos[0].parentElement.parentElement)
-console.log(cursos[0].parentElement.parentElement.parentElement)
-console.log(cursos[0].parentElement.parentElement.parentElement.children)
-console.log(cursos[0].parentElement.parentElement.parentElement.children[0])
- console.log(cursos[0].parentElement.parentElement.parentElement.children[0].textContent='Hola desde traversing')
+//Agregarlo al HTML
 
- */
-
- const enlaces = document.querySelectorAll('.enlace')
-
- console.log(enlaces[4]) //estamos en temas ---parte baja de la pagina
- //deseamos subir a soporte
- console.log(enlaces[4].previousSibling)//mostrará el espacio en blanco como text en html
- console.log(enlaces[4].previousElementSibling)//Estaremos ubicados en soporte
-
- //sibling son los que se encuentran al mismo nivel, que no esten endientados ej.
-
- /*
-    <div class="four columns">
-                            <nav id="principal" class="menu">
-                                <a class="enlace" href="#">Para tu Negocio</a>          ------
-                                <a class="enlace" href="#">Conviertete en Instructor</a> ----
-                                <a class="enlace" href="#">Aplicaciones Móviles</a>     ------
-                                <a class="enlace" href="#">Soporte</a>                   -----
-                                <a class="enlace" href="#">Temas</a>                    ------
-                            </nav>
-                    </div>
- */
-console.log(enlaces[4].previousElementSibling.previousElementSibling) //aplicaciones moviles
+document.querySelector('#secundaria').appendChild(enlace) //seleccionamos la ubicacion a insertar y luego con appendChild inyectamos el elemento desde javascript
 
 
-
-// ahora de arriba hacia abajo --empezariamos en para tu negocio
-
-console.log(enlaces[0].nextElementSibling) //llegaria a Conviertete en instructor
-console.log(enlaces[0].nextElementSibling.nextElementSibling.parentElement) //llegaria a la etiqueta nav
-console.log(enlaces[0].nextElementSibling.nextElementSibling.parentElement.children)//lista hijos
-console.log(enlaces[0].nextElementSibling.nextElementSibling.parentElement.children[4])
-
+console.log(enlace)
