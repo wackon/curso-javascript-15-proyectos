0ed1988e42b83b470a6fdad9269bdde35b14408e
wackon <jeac86@gmail.com>
2020-06-26 13:34:35 -0500

    cap 3 video 50

--- cap 3/universidad/js/app.js
+++ cap 3/universidad/js/app.js
@@ -1,60 +1,57 @@
-//Traversing
-
-
-//muestra los nodos y cuenta tambien los espacios 
+//traversing del hijo al padre
 
 /*
-const navegacion = document.querySelector('#principal')
 
+const enlaces = document.querySelectorAll('.enlace')
 
-console.log(navegacion.childNodes);
+console.log(enlaces[0].parentNode) //parentNode : lo guia hacia el padre de donde nos encontramos
+console.log(enlaces[0].parentElement)//hace lo mismo que el anterior, pero este es el mas usado
+
+console.log(enlaces[0].parentElement.parentElement.parentElement)//suebe cada vez mas hacia su padre
 
 */
 
-//muestra solo los nodos con informacion
-
-const navegacion = document.querySelector('#principal')
-
-console.log(navegacion.children[0]);
-console.log(navegacion.nodeName)
-console.log(navegacion.children[0].textContent); // muestra el contenido en texto de la etiqueta
-console.log(navegacion.children[0].nodeName);//muestra a que es la etiqueta del html
-console.log(navegacion.children[0].nodeType); //los tipos de nodos regresan en numeros 
-
-/*  tipo 1 = Elementos html
-    tipo 2 = Atributos
-    tipo 3 = Texto
-    tipo 8 = comentarios
-    tipo 9 = documentos
-    tipo 10 = doctype
-
-    todos conforman el html
-
-    */
-///////////////////////////////////////////////
-    
-console.log(navegacion.children[0].textContent = "nuevo enlace");
-
-
-const barra = document.querySelector('.barra')
-
-
-console.log(barra.children[0].children[0].children);
-
-////////////////////////////////////////////
-
-
-///////////////////////////////////////
-
+/*
 const cursos = document.querySelectorAll('.card')
 
+console.log(cursos[0])
+console.log(cursos[0].parentElement)
+console.log(cursos[0].parentElement.parentElement)
+console.log(cursos[0].parentElement.parentElement.parentElement)
+console.log(cursos[0].parentElement.parentElement.parentElement.children)
+console.log(cursos[0].parentElement.parentElement.parentElement.children[0])
+ console.log(cursos[0].parentElement.parentElement.parentElement.children[0].textContent='Hola desde traversing')
 
-console.log(cursos[0].lastChild);//devuelve texto lo que indica que son espacios
+ */
 
-console.log(cursos[0].lastElementChild) //devuelve el elemento hijo que se encuentra en la ultima posicion
+ const enlaces = document.querySelectorAll('.enlace')
 
-console.log(cursos[0].firstElementChild)//devuelve el elemento hijo en pocision 0
+ console.log(enlaces[4]) //estamos en temas ---parte baja de la pagina
+ //deseamos subir a soporte
+ console.log(enlaces[4].previousSibling)//mostrará el espacio en blanco como text en html
+ console.log(enlaces[4].previousElementSibling)//Estaremos ubicados en soporte
 
-console.log(cursos[0].childElementCount);//muestra la cantidad de elementos hijos, en este caso 2 - pos 0 la imagen , pos 1 info card
+ //sibling son los que se encuentran al mismo nivel, que no esten endientados ej.
 
-//////////////////////////////////
+ /*
+    <div class="four columns">
+                            <nav id="principal" class="menu">
+                                <a class="enlace" href="#">Para tu Negocio</a>          ------
+                                <a class="enlace" href="#">Conviertete en Instructor</a> ----
+                                <a class="enlace" href="#">Aplicaciones Móviles</a>     ------
+                                <a class="enlace" href="#">Soporte</a>                   -----
+                                <a class="enlace" href="#">Temas</a>                    ------
+                            </nav>
+                    </div>
+ */
+console.log(enlaces[4].previousElementSibling.previousElementSibling) //aplicaciones moviles
+
+
+
+// ahora de arriba hacia abajo --empezariamos en para tu negocio
+
+console.log(enlaces[0].nextElementSibling) //llegaria a Conviertete en instructor
+console.log(enlaces[0].nextElementSibling.nextElementSibling.parentElement) //llegaria a la etiqueta nav
+console.log(enlaces[0].nextElementSibling.nextElementSibling.parentElement.children)//lista hijos
+console.log(enlaces[0].nextElementSibling.nextElementSibling.parentElement.children[4])
+
