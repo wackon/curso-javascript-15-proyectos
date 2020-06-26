a0c715ce82b4817f7d1cfaa3a1673cc458033ac7
wackon <jeac86@gmail.com>
2020-06-26 14:51:01 -0500

    cap 3 video 53

--- cap 3/universidad/js/app.js
+++ cap 3/universidad/js/app.js
@@ -1,26 +1,58 @@
-//remplazar o modificar elementos desde javascript
+//Eliminar elementos desde javascript--estos pueden ser borrados sobre si mismmo ó desde el padre
 
-const nuevoEncabezado = document.createElement('h2')
+/*
+const enlaces = document.querySelectorAll('.enlace')
 
-//agregar un id
-nuevoEncabezado.id = 'encabezado';
+const navegacion = document.querySelector('#principal')
 
-//agregar nuevo texto
-nuevoEncabezado.appendChild(document.createTextNode('Los Mejores Cursos'))
+//enlaces[0].remove();//hemos borrado para tu negocio desde él mismo
 
-console.log(nuevoEncabezado)
 
-//Elemento anterior será remplazado
+console.log(enlaces)
+console.log(navegacion)
 
-const anterior = document.querySelector('#encabezado')
+*/
 
-console.log(anterior)
-console.log(anterior.parentElement)//se tomará esta referencia para realizar el cambio, ya que este es el padre del elemento a remplazar
+//borrar desde el padre
 
-//elemento padre
-const elPadre = document.querySelector('#lista-cursos');
+/*
+const enlaces = document.querySelectorAll('.enlace')
 
-//remplazamos codigo
+const navegacion = document.querySelector('#principal')
 
-elPadre.replaceChild(nuevoEncabezado, anterior)
+navegacion.removeChild(enlaces[0])
 
+
+console.log(enlaces)
+console.log(navegacion)
+
+*/
+
+const primerLi = document.querySelector('.enlace')
+
+let elemento;
+
+//obtener una clase de CSS
+elemento = primerLi.className;
+elemento = primerLi.classList;
+
+//añadimos una nueva clase
+
+elemento = primerLi.classList.add('nueva-clase')//crea la nueva clase
+elemento = primerLi.classList.remove('nueva-clase')//elimina la clase
+
+elemento = primerLi.classList;
+
+//leer atributos
+elemento = primerLi.getAttribute('href') //obtenemos el valor
+primerLi.setAttribute('href','http://facebook.com')//cambiamos el valor del atributo
+primerLi.setAttribute('data-id',20) //adicionamos valor a un nuevo atributo
+
+elemento = primerLi.hasAttribute('data-id');//valida si el atriburo data-id existe 
+primerLi.removeAttribute('data-id')//remueve el atributo data-id
+
+elemento = primerLi;
+
+
+
+console.log(elemento)
